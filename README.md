8685822d42950a9b3bda1c496acabc39a0847591
wushufen <wusfun@foxmail.com>
2021-12-08 15:34:45 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# commit-lint
+
+在提交代码时，自动对提交的文件进行代码校验。
+
+## 安装
+
+```bash
+yarn add -D https://github.com.cnpmjs.org/wushufen/commit-lint.git
+```
+
+```bash
+npm i -D git+https://github.com.cnpmjs.org/wushufen/commit-lint.git
+```
---
+++ install.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+
+const targetPath = `${__dirname}/../../`
+const preCommitPath = `${targetPath}/.git/hooks/pre-commit`
+const outputFileName = `commit-lint`
+
+// backup
+if (fs.existsSync(preCommitPath)) {
+  fs.writeFileSync(`${preCommitPath}.backup`, fs.readFileSync(preCommitPath))
+}
+
+// write .git/hooks/pre-commit
+fs.writeFileSync(
+  preCommitPath,
+  `#!/usr/bin/env bash
+if [ -f "${outputFileName}" ];then . "${outputFileName}";fi
+`
+)
+
+// write lint file
+fs.writeFileSync(
+  `${targetPath}/${outputFileName}`,
+  fs.readFileSync(`${__dirname}/commit-lint`)
+)
