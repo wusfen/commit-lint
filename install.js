8886870a84e04f52f268b93c1b95f406a5992088
wushufen <wusfun@foxmail.com>
2021-12-09 15:16:34 +0800

    try catch

--- install.js
+++ install.js
@@ -1,24 +1,28 @@
-const fs = require('fs')
+try {
+  const fs = require('fs')
 
-const targetPath = `${__dirname}`.replace(/[\\/]node_modules.*/, '')
-const preCommitPath = `${targetPath}/.git/hooks/pre-commit`
-const outputFileName = `commit-lint`
+  const targetPath = `${__dirname}`.replace(/[\\/]node_modules.*/, '')
+  const preCommitPath = `${targetPath}/.git/hooks/pre-commit`
+  const outputFileName = `commit-lint`
 
-// backup
-if (fs.existsSync(preCommitPath)) {
-  fs.writeFileSync(`${preCommitPath}.backup`, fs.readFileSync(preCommitPath))
-}
+  // backup
+  if (fs.existsSync(preCommitPath)) {
+    fs.writeFileSync(`${preCommitPath}.backup`, fs.readFileSync(preCommitPath))
+  }
 
-// write .git/hooks/pre-commit
-fs.writeFileSync(
-  preCommitPath,
-  `#!/usr/bin/env bash
+  // write .git/hooks/pre-commit
+  fs.writeFileSync(
+    preCommitPath,
+    `#!/usr/bin/env bash
 if [ -f "${outputFileName}" ];then . "${outputFileName}";fi
 `
-)
+  )
 
-// write lint file
-fs.writeFileSync(
-  `${targetPath}/${outputFileName}`,
-  fs.readFileSync(`${__dirname}/commit-lint`)
-)
+  // write lint file
+  fs.writeFileSync(
+    `${targetPath}/${outputFileName}`,
+    fs.readFileSync(`${__dirname}/commit-lint`)
+  )
+} catch (error) {
+  console.warn('[commit-lint]', error)
+}
