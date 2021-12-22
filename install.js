ff00e5334bf0b66f9390a39bb17e25772759f509
wushufen <wusfun@foxmail.com>
2021-12-22 17:36:00 +0800

    fix: mkdirSync .git/hooks

--- install.js
+++ install.js
@@ -2,8 +2,9 @@ try {
   const fs = require('fs')
 
   const targetPath = `${__dirname}`.replace(/[\\/]node_modules.*/, '')
-  const preCommitPath = `${targetPath}/.git/hooks/pre-commit`
-  const outputFileName = `commit-lint`
+  const hooksPath = `${targetPath}/.git/hooks`
+  const preCommitPath = `${hooksPath}/pre-commit`
+  const shellFileName = `commit-lint`
 
   // backup
   if (fs.existsSync(preCommitPath)) {
@@ -11,17 +12,18 @@ try {
   }
 
   // write .git/hooks/pre-commit
+  fs.mkdirSync(`${preCommitPath}/..`, {recursive: true}) // ensure
   fs.writeFileSync(
     preCommitPath,
     `#!/usr/bin/env bash
-if [ -f "${outputFileName}" ];then . "${outputFileName}";fi
+if [ -f "${shellFileName}" ];then . "${shellFileName}";fi
 `
   )
 
   // write lint file
   fs.writeFileSync(
-    `${targetPath}/${outputFileName}`,
-    fs.readFileSync(`${__dirname}/commit-lint`)
+    `${targetPath}/${shellFileName}`,
+    fs.readFileSync(`${__dirname}/${shellFileName}`)
   )
 } catch (error) {
   console.warn('[commit-lint]', error)
